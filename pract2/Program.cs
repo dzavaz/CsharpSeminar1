824ec8510341b94e5db293128121912abce2445a
Dzmitry <dimastik98.ru@mail.ru>
2023-09-23 23:48:31 +0300

    добавление кода  pract2

--- pract2/Program.cs
+++ pract2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int с = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(с > max) max = с;
+
+Console.Write("max = ");
+Console.WriteLine(max);
