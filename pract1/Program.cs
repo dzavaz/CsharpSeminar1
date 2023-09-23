af458ea52dcaac2cce3e32712eeb64c0a04e65c9
Dzmitry <dimastik98.ru@mail.ru>
2023-09-23 23:39:34 +0300

    добавление кода  pract1

--- pract1/Program.cs
+++ pract1/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
